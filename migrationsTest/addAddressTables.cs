9edcff91c45eafddedfedce2a5a51ae14e0eac3d
Br0der99 <1083570@ucn.dk>
2021-05-08 16:56:42 +0200

    Did Migrations shit for Room and Address

---
+++ migrationsTest/addAddressTables.cs
@@ -0,0 +1,55 @@
+﻿using FluentMigrator;
+
+namespace migrationsTest
+{
+    [Migration(1)]
+    public class AddAddressTables : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("city")
+                .WithColumn("id").AsInt64().Identity().PrimaryKey()
+                .WithColumn("name").AsString().Unique();
+
+            Create.Table("cityZipcode")
+                .WithColumn("id").AsInt64().Identity().PrimaryKey()
+                .WithColumn("cityId").AsInt64().Unique()
+                .WithColumn("zipCode").AsInt32().Unique();
+
+            Create.ForeignKey("cityZipode_cityId_city_id")
+                .FromTable("cityZipcode").ForeignColumn("cityId")
+                .ToTable("city").PrimaryColumn("id");
+
+            Create.Table("street")
+                .WithColumn("id").AsInt64().Identity().PrimaryKey()
+                .WithColumn("name").AsString().NotNullable().Unique();
+
+            Create.Table("streetNumber")
+                .WithColumn("id").AsInt64().Identity().PrimaryKey()
+                .WithColumn("streetId").AsInt64().Unique()
+                .WithColumn("streetNumber").AsInt64().Unique();
+
+            Create.ForeignKey("streetNumber_streetId_street_id")
+                .FromTable("streetNumber").ForeignColumn("streetId")
+                .ToTable("street").PrimaryColumn("id");
+
+            Create.Table("address")
+                .WithColumn("id").AsInt64().Identity().PrimaryKey()
+                .WithColumn("streetNumberId").AsInt64().Unique()
+                .WithColumn("cityZipId").AsInt64().Unique();
+
+            Create.ForeignKey("address_streetNumberId_streetNumber")
+                .FromTable("address").ForeignColumn("streetNumberId")
+                .ToTable("streetNumber").PrimaryColumn("id");
+
+            Create.ForeignKey("address_cityZipId_cityZipcode_id")
+                .FromTable("address").ForeignColumn("cityZipId")
+                .ToTable("cityZipcode").PrimaryColumn("id");
+        }
+
+        public override void Down()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ migrationsTest/addRoomTable.cs
@@ -0,0 +1,32 @@
+﻿using FluentMigrator;
+
+namespace migrationsTest
+{
+    [Migration(2)]
+    public class AddRoomTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("RoomType")
+                .WithColumn("id").AsInt64().Identity().PrimaryKey()
+                .WithColumn("roomType").AsString().Unique();
+
+
+            Create.Table("Room")
+                .WithColumn("id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("numberOfBeds").AsInt64().NotNullable()
+                .WithColumn("price").AsInt64().NotNullable()
+                .WithColumn("roomTypeId").AsInt64().NotNullable();
+
+            Create.ForeignKey("fk_Room_roomTypeId_RoomType_Id")
+                .FromTable("Room").ForeignColumn("roomTypeId")
+                .ToTable("RoomType").PrimaryColumn("id");
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Room");
+            Delete.Table("RoomType");
+        }
+    }
+}
