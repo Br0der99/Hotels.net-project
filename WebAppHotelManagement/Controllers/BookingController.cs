9fecf2798ab3857816174e4599ecd93ccd262ed4
Bjarke Robert Støve Andersen <1078101@ucn.dk>
2021-05-15 17:43:19 +0200

    Booking

---
+++ WebAppHotelManagement/Controllers/BookingController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppHotelManagement.Models;
+using WebAppHotelManagement.ViewModel;
+
+namespace WebAppHotelManagement.Controllers
+{
+    public class BookingController : Controller
+    {
+        private HotelDBEntities objHotelDBEntities;
+        public BookingController()
+        {
+            objHotelDBEntities = new HotelDBEntities();
+        }
+
+        public ActionResult Index()
+        {
+            BookingViewModel objBookingViewModel = new BookingViewModel();
+            objBookingViewModel.ListOfRoom = (from objRoom in objHotelDBEntities.Rooms
+                                              where objRoom.BookingStatusId == 2
+                                              select new SelectListItem()
+                                              {
+                                                  Text = objRoom.RoomNumber,
+                                                  Value = objRoom.RoomId.ToString()
+                                              }
+                                              ).ToList();
+            objBookingViewModel.BookingFrom = DateTime.Now;
+            objBookingViewModel.BookingTo = DateTime.Now.AddDays(1);
+
+            return View(objBookingViewModel);
+        }
+        [HttpPost]
+        public ActionResult Index(BookingViewModel objBookingViewModel)
+        {
+            int numberOfDays = Convert.ToInt32((objBookingViewModel.BookingTo - objBookingViewModel.BookingFrom).TotalDays);
+            Rooms objRoom = objHotelDBEntities.Rooms.Single(model => model.RoomId == objBookingViewModel.AssignRoomId);
+            decimal RoomPrice = objRoom.RoomPrice;
+            decimal TotalAmount = RoomPrice * numberOfDays;
+
+            string dt = objBookingViewModel.BookingTo.ToString(format: "MM/dd/yyyy");
+            RoomBookings roomBookings = new RoomBookings()
+            {
+                //  BookingId = objBookingViewModel.BookingId,
+                AssignRoomId = objBookingViewModel.AssignRoomId,
+                CustomerName = objBookingViewModel.CustomerName,
+                CustomerAddress = objBookingViewModel.CustomerAddress,
+                CustomerPhone = objBookingViewModel.CustomerPhone,
+                BookingFrom = objBookingViewModel.BookingFrom,
+                BookingTo = objBookingViewModel.BookingTo,
+                NoOfMembers = objBookingViewModel.NumberOfMembers,
+                TotalAmount = TotalAmount
+            };
+            objHotelDBEntities.RoomBookings.Add(roomBookings);
+            objHotelDBEntities.SaveChanges();
+
+            objRoom.BookingStatusId = 3;
+            objHotelDBEntities.SaveChanges();
+
+            return Json(new { message = "Hotel Booking is Successfully Created.", success = true }, JsonRequestBehavior.AllowGet);
+
+        }
+        [HttpGet]
+        public PartialViewResult GetAllBookingHistory()
+        {
+            List<RoomBookingViewModel> listOfBookingViewModels = new List<RoomBookingViewModel>();
+            listOfBookingViewModels = (from objHotelBooking in objHotelDBEntities.RoomBookings
+                                       join objRoom in objHotelDBEntities.Rooms on objHotelBooking.AssignRoomId equals objRoom.RoomId
+                                       select new RoomBookingViewModel()
+                                       {
+                                           BookingFrom = objHotelBooking.BookingFrom,
+                                           BookingTo = objHotelBooking.BookingTo,
+                                           CustomerPhone = objHotelBooking.CustomerPhone,
+                                           CustomerName = objHotelBooking.CustomerName,
+                                           TotalAmount = objHotelBooking.TotalAmount,
+                                           CustomerAddress = objHotelBooking.CustomerAddress,
+                                           NumberOfMembers = objHotelBooking.NoOfMembers,
+                                           BookingId = objHotelBooking.BookingId,
+                                           RoomNumber = objRoom.RoomNumber,
+                                           RoomPrice = objRoom.RoomPrice,
+                                           NumberOfDays = System.Data.Entity.DbFunctions.DiffDays(objHotelBooking.BookingFrom, objHotelBooking.BookingTo).Value
+
+                                       }).ToList();
+            return PartialView("_BookingHistoryPartial", listOfBookingViewModels);
+        }
+    }
+}
---
+++ WebAppHotelManagement/ViewModel/BookingViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppHotelManagement.ViewModel
+{
+    public class BookingViewModel
+    {
+        //   public int BookingId { get; set; }
+        [Display(Name = "Customer Name")]
+        [Required(ErrorMessage = "Customer name is required.")]
+        public string CustomerName { get; set; }
+
+        [Display(Name = "Customer Address")]
+        [Required(ErrorMessage = "Customer Address is required.")]
+        public string CustomerAddress { get; set; }
+
+        [Display(Name = "Customer Phone")]
+        [Required(ErrorMessage = "Customer Phone is required.")]
+        public string CustomerPhone { get; set; }
+        //DateTime skal være Date uden Time for at fremvise en kalender. Men så er outputtet yyyy-MM-dd vidst standard pga noget US. Og det fungere ikke som EU.
+        [Display(Name = "Booking From")]
+        [Required(ErrorMessage = "Booking From is required.")]
+        [DisplayFormat(DataFormatString = "{0:dd-MMM-yyyy}", ApplyFormatInEditMode = true)]
+        //[DataType(DataType.Date)]
+        public DateTime BookingFrom { get; set; }
+
+        //DateTime skal være Date uden Time for at fremvise en kalender. Men så er outputtet yyyy-MM-dd vidst standard pga noget US. Og det fungere ikke som EU.
+        [Display(Name = "Booking To")]
+        [Required(ErrorMessage = "Booking To is required.")]
+        [DisplayFormat(DataFormatString = "{0:dd-MMM-yyyy}", ApplyFormatInEditMode = true)]
+        //[DataType(DataType.Date)]
+        public DateTime BookingTo { get; set; }
+
+        [Display(Name = "Assign Room")]
+        [Required(ErrorMessage = "Room is required.")]
+        public int AssignRoomId { get; set; }
+
+        [Display(Name = "Number Of Members")]
+        [Required(ErrorMessage = "Number of members is required.")]
+        public int NumberOfMembers { get; set; }
+        public IEnumerable<SelectListItem> ListOfRoom { get; set; }
+    }
+}
