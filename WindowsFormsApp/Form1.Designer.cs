32dc2e0cf044cf27554b693b9898b8f8295d0f2e
Bjarke Robert Støve Andersen <1078101@ucn.dk>
2021-05-15 15:21:57 +0200

    Indsætning af Dapper Form CRUD for Rooms

---
+++ WindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace WindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.dgvEmployee = new System.Windows.Forms.DataGridView();
+            this.lblMobile = new System.Windows.Forms.Label();
+            this.txtMobile = new System.Windows.Forms.TextBox();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployee)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblName.Location = new System.Drawing.Point(32, 29);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(49, 19);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtName.Location = new System.Drawing.Point(111, 26);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(329, 26);
+            this.txtName.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnSave.Location = new System.Drawing.Point(12, 279);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // dgvEmployee
+            // 
+            this.dgvEmployee.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmployee.Location = new System.Drawing.Point(506, 65);
+            this.dgvEmployee.Name = "dgvEmployee";
+            this.dgvEmployee.ReadOnly = true;
+            this.dgvEmployee.RowTemplate.Height = 25;
+            this.dgvEmployee.Size = new System.Drawing.Size(509, 290);
+            this.dgvEmployee.TabIndex = 3;
+            this.dgvEmployee.DoubleClick += new System.EventHandler(this.dgvEmployee_DoubleClick);
+            // 
+            // lblMobile
+            // 
+            this.lblMobile.AutoSize = true;
+            this.lblMobile.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblMobile.Location = new System.Drawing.Point(32, 65);
+            this.lblMobile.Name = "lblMobile";
+            this.lblMobile.Size = new System.Drawing.Size(56, 19);
+            this.lblMobile.TabIndex = 0;
+            this.lblMobile.Text = "Mobile";
+            // 
+            // txtMobile
+            // 
+            this.txtMobile.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtMobile.Location = new System.Drawing.Point(111, 62);
+            this.txtMobile.Name = "txtMobile";
+            this.txtMobile.Size = new System.Drawing.Size(329, 26);
+            this.txtMobile.TabIndex = 1;
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblAddress.Location = new System.Drawing.Point(32, 107);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(66, 19);
+            this.lblAddress.TabIndex = 0;
+            this.lblAddress.Text = "Address";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtAddress.Location = new System.Drawing.Point(111, 104);
+            this.txtAddress.Multiline = true;
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(329, 135);
+            this.txtAddress.TabIndex = 1;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnDelete.Location = new System.Drawing.Point(111, 279);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCancel.Location = new System.Drawing.Point(207, 279);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(629, 29);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(100, 23);
+            this.txtSearch.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(759, 29);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1027, 477);
+            this.Controls.Add(this.dgvEmployee);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtMobile);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.lblMobile);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
+            this.Text = "Employee Contact Book";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployee)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DataGridView dgvEmployee;
+        private System.Windows.Forms.Label lblMobile;
+        private System.Windows.Forms.TextBox txtMobile;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ WindowsFormsApp/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source = .; Initial Catalog = DapperDB; Integrated Security = True");
+        int empId = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+                DynamicParameters param = new DynamicParameters();
+                param.Add("EmpID", empId);
+                param.Add("@Name", txtName.Text.Trim());
+                param.Add("@Mobile", txtMobile.Text.Trim());
+                param.Add("@Address", txtAddress.Text.Trim());
+                con.Execute("EmpAddOrEdit", param, commandType: CommandType.StoredProcedure);
+                if (empId == 0)
+                    MessageBox.Show("Saved Succesfully");
+                else
+                    MessageBox.Show("Updated Successfully");
+                FillDataGridView();
+                Clear();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+
+        void FillDataGridView()
+        {
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@SearchText", txtSearch.Text.Trim());
+            List<Employee> list = con.Query<Employee>("EmpViewOrSearch", param, commandType: CommandType.StoredProcedure).ToList<Employee>();
+            dgvEmployee.DataSource = list;
+            dgvEmployee.Columns[0].Visible = false;
+        }
+        class Employee
+        {
+            public int EmpID { get; set; }
+            public string Name { get; set; }
+            public string Mobile { get; set; }
+            public string Address { get; set; }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FillDataGridView();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                FillDataGridView();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            Clear();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        void Clear()
+        {
+            txtName.Text = txtMobile.Text = txtAddress.Text = txtSearch.Text = "";
+            empId = 0;
+            btnSave.Text = "Save";
+            btnDelete.Enabled = false;
+        }
+
+        private void dgvEmployee_DoubleClick(object sender, EventArgs e)
+        {
+            try
+            {
+                if(dgvEmployee.CurrentRow.Index != -1)
+                {
+                    empId = Convert.ToInt32(dgvEmployee.CurrentRow.Cells[0].Value.ToString());
+                    txtName.Text = dgvEmployee.CurrentRow.Cells[1].Value.ToString();
+                    txtMobile.Text = dgvEmployee.CurrentRow.Cells[2].Value.ToString();
+                    txtAddress.Text = dgvEmployee.CurrentRow.Cells[3].Value.ToString();
+                    btnSave.Text = "Update";
+                    btnDelete.Enabled = true;
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(MessageBox.Show("Are you sure to delete this record?", "Message", MessageBoxButtons.YesNo) ==DialogResult.Yes)
+                {
+                    DynamicParameters param = new DynamicParameters();
+                    param.Add("@EmpID", empId);
+                    con.Execute("EmpDeleteByID", param, commandType: CommandType.StoredProcedure);
+                    Clear();
+                    FillDataGridView();
+                    MessageBox.Show("Deleted Successfully");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
