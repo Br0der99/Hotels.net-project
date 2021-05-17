84a1215e71c798043b93fc1de1b2f085fdf1752c
Bjarke Robert Støve Andersen <1078101@ucn.dk>
2021-05-17 12:08:04 +0200

    Refactor

--- Model/BookingStatus.cs
+++ Model/BookingStatus.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Model
 {
-    class BookingStatus
+   public class BookingStatus
     {
         public int Id { get; set; }
         public string BookingStatusName { get; set; }
